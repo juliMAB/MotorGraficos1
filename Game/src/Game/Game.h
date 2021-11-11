36f36baeea58f8928240a6d6c8e6d4222125776c
juliMAB <julian6aguirre@hotmail.com>
2021-11-11 10:25:37 -0300

    GAME LOOP

--- Game/src/Game/Game.h
+++ Game/src/Game/Game.h
@@ -5,17 +5,7 @@ namespace Coco {
 	
 	class Game : GameBase {
 		Shape* _shape;
-		Shape* _shape2;
-		Shape* _shape3;
 		Camera* _camera;
-		Material* _goldMaterial;
-		Material* _obsidianMaterial;
-		Model* _model1;
-		Model* _model2;
-		Model* _model3;
-		Model* _model4;
-		Model* _model5;
-		Model* _model6;
 	public:
 		Game();
 		~Game();
